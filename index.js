abf38e50d090c9ae1cc5ecbf057909aea6937abc
Shelby Morris <shelbynicole.morris@gmail.com>
2023-02-06 19:18:42 -0700

    complete tests

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+// function setBestCustomer() {
+//     var bestCustomer = 'not bob';
+//     return bestCustomer;
+// }
+
+// function setBestCustomer() {
+//     var bestCustomer = 'not bob';
+//     console.log(bestCustomer);
+// }
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'janice';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'suzie';
+}
